143315487e3e3d52889505b06a818a78d8db41ba
Haowei <1311304948@qq.com>
2025-02-11 21:38:32 +0800

    eICU AKI

--- Concepts/ICU_AKI/eICU/sql/label/main.sql
+++ Concepts/ICU_AKI/eICU/sql/label/main.sql
@@ -1,12 +1,12 @@
 -- KDIGO
     -- Cretinine
-\i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_48h.sql'
-\i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_7d.sql'
-\i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_baseline.sql'
+-- \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_48h.sql'
+-- \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_7d.sql'
+-- \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/cr_baseline.sql'
 \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/aki_cr.sql'
     -- Urine Output
-\i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/patient_weight.sql'
-\i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/pivoted_uo.sql'
+-- \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/patient_weight.sql'
+-- \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/pivoted_uo.sql'
 \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/aki_uo.sql'
 -- RRT
 \i '/data/hwxu/Dataset/PKU/AwsomeEHR/Concepts/ICU_AKI/eICU/sql/label/aki_rrt.sql'
