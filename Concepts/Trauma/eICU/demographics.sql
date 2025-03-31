8af7797a51448030da357c71f608e14bf4dbe479
Tory Shi <157093931+STYAI@users.noreply.github.com>
2025-03-31 11:09:30 +0800

    Update demographics.sql

--- Concepts/Trauma/eICU/demographics.sql
+++ Concepts/Trauma/eICU/demographics.sql
@@ -1,14 +1,40 @@
 -- 如果表存在则删除
-DROP TABLE IF EXISTS eicu_crd.trauma_demographics;
+DROP TABLE IF EXISTS public.trauma_patients CASCADE;
+
+-- 创建新表并插入数据
+CREATE TABLE public.trauma_patients AS
+-- SELECT DISTINCT p.uniquepid
+-- FROM eicu.patient p
+-- JOIN eicu.admissiondx a ON p.patientunitstayid = a.patientunitstayid
+-- WHERE p.unittype IN ('SICU', 'Med-Surg ICU')
+-- AND (
+--     LOWER(a.admitdxname) LIKE '%trauma%'
+--     OR LOWER(a.admitdxname) LIKE '%burn%'
+-- );
+SELECT DISTINCT p.patientunitstayid
+FROM eicu.patient p
+JOIN eicu.admissiondx a ON p.patientunitstayid = a.patientunitstayid
+WHERE p.unittype IN ('SICU', 'Med-Surg ICU')
+AND (
+    LOWER(a.admitdxname) LIKE '%trauma%'
+    OR LOWER(a.admitdxname) LIKE '%burn%'
+)
+AND p.unitvisitnumber = 1;
+
+
+
+
+-- 如果表存在则删除
+DROP TABLE IF EXISTS public.trauma_demographics;
 
 -- 创建人口统计学特征表
-CREATE TABLE eicu_crd.trauma_demographics AS
+CREATE TABLE public.trauma_demographics AS
 SELECT DISTINCT 
-    tp.uniquepid,
+    tp.patientunitstayid,
     pt.age,
     CASE WHEN pt.gender = 'Male' THEN 1
-         WHEN pt.gender = 'Female' THEN 2
+         WHEN pt.gender = 'Female' THEN 0
          ELSE NULL END AS gender,
     pt.ethnicity as race
-FROM eicu_crd.trauma_patients tp
-JOIN eicu_crd.patient pt ON tp.uniquepid = pt.uniquepid;
+FROM public.trauma_patients tp
+JOIN eicu.patient pt ON tp.patientunitstayid = pt.patientunitstayid;
